263e2c0aa7f27efdf899920fe43ac7592e8788f7
Aidan Prehatny <aprehatny@yahoo.com>
2019-03-16 18:58:21 -0700

    console syntax

---
+++ src/print.rs
@@ -0,0 +1,13 @@
+pub fn run() {
+    // print from console
+    print ln!("Gucci Prada");
+
+    // basic format
+    println!("{} is {}", "gucci", "cool");
+
+    // positional arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Tom", "Hollywood", "program"
+    )
+}
