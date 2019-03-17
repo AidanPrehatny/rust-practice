5a299514eeaa470e26eff2eb53f2669c27b503d5
Aidan Prehatny <aprehatny@yahoo.com>
2019-03-16 19:01:00 -0700

    add printr mod to main execute run

--- src/print.rs
+++ src/print.rs
@@ -1,6 +1,6 @@
 pub fn run() {
     // print from console
-    print ln!("Gucci Prada");
+    println!("Gucci Prada");
 
     // basic format
     println!("{} is {}", "gucci", "cool");
@@ -9,5 +9,10 @@ pub fn run() {
     println!(
         "{0} is from {1} and {0} likes to {2}",
         "Tom", "Hollywood", "program"
-    )
+    );
+
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+    println!("{:?}", (12, true, "hello"));
+
 }
